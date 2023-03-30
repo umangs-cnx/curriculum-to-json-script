729ed5b9477f721798ac2a466467856934545957
Umang Sharma <umang.sharma@cybernetyx.com>
2023-03-30 18:59:08 +0530

    Setting the default start script for the start function to be called.

--- exportToJSON.js
+++ exportToJSON.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 class ExportToJSON {
   writeToFile(jsonData) {
-    fs.writeFile("test.json", jsonData, function(err) {
+    fs.writeFile("output.json", jsonData, function(err) {
       if (err) {
         console.log(err);
       }
